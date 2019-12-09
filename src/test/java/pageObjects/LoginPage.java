bba7c751f3b97e538c713d2f78de02d7c512b5a4
avanghelovici <artur.vanghelovici@endava.com>
2019-12-09 15:38:12 +0200

    creating page objects Artur

---
+++ src/test/java/pageObjects/LoginPage.java
@@ -0,0 +1,34 @@
+package pageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPage {
+    @FindBy(xpath = "//*[@id=\"login-username\"]")
+    private WebElement eMailField;
+
+    @FindBy(xpath = "//*[@id=\"login-password\"]")
+    private WebElement passwordField;
+
+    @FindBy(xpath = "//*[@id=\"loginSubmitButton\"]")
+    private WebElement loginButton;
+
+    @FindBy(xpath = "//*[@id=\"container\"]/div[1]/div/div/div/span")
+    private WebElement notValidCredentials;
+
+    public WebElement getEMailField() {
+        return eMailField;
+    }
+
+    public WebElement getPasswordField() {
+        return passwordField;
+    }
+
+    public WebElement getLoginButton() {
+        return loginButton;
+    }
+
+    public WebElement getNotValidCredentials() {
+        return notValidCredentials;
+    }
+}
---
+++ src/test/java/pageObjects/QuickTruckSearch.java
@@ -0,0 +1,76 @@
+package pageObjects;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class QuickTruckSearch {
+    @FindBy(xpath = "//*[@id=\"rbt-VUT75-link\"]/i")
+    private WebElement vansNtRucksUpTo7p5t;
+
+    @FindBy(xpath = "//*[@id=\"rbt-TO75-link\"]/i")
+    private WebElement trucksOver7p5t;
+
+    @FindBy(xpath = "//*[@id=\"rbt-STT-link\"]/i")
+    private WebElement semiTrailerTruck;
+
+    @FindBy(xpath = "//*[@id=\"rbt-T-link\"]/i")
+    private WebElement trailersCategory;
+
+    @FindBy(xpath = "//*[@id=\"rbt-ST-link\"]/i")
+    private WebElement semiTrailers;
+
+    @FindBy(xpath = "//*[@id=\"rbt-CM-link\"]/i")
+    private WebElement constructionMachines;
+
+    @FindBy(xpath = "//*[@id=\"rbt-B-link\"]/i")
+    private WebElement busesCategory;
+
+    @FindBy(xpath = "//*[@id=\"rbt-AV-link\"]/i")
+    private WebElement agriculturalVehicles;
+
+    @FindBy(xpath = "//*[@id=\"rbt-FT-link\"]/i")
+    private WebElement forkliftTrucks;
+
+    @FindBy(xpath = "//*[@id=\"form-dsp\"]/div[1]/div/article/section/div/div[1]/h1")
+    private WebElement generalTrucksCategoryHeader;
+
+    public WebElement getVansNtRucksUpTo7p5t() {
+        return vansNtRucksUpTo7p5t;
+    }
+
+    public WebElement getTrucksOver7p5t() {
+        return trucksOver7p5t;
+    }
+
+    public WebElement getSemiTrailerTruck() {
+        return semiTrailerTruck;
+    }
+
+    public WebElement getTrailersCategory() {
+        return trailersCategory;
+    }
+
+    public WebElement getSemiTrailers() {
+        return semiTrailers;
+    }
+
+    public WebElement getConstructionMachines() {
+        return constructionMachines;
+    }
+
+    public WebElement getBusesCategory() {
+        return busesCategory;
+    }
+
+    public WebElement getAgriculturalVehicles() {
+        return agriculturalVehicles;
+    }
+
+    public WebElement getForkliftTrucks() {
+        return forkliftTrucks;
+    }
+
+    public WebElement getGeneralTrucksCategoryHeader() {
+        return generalTrucksCategoryHeader;
+    }
+}
