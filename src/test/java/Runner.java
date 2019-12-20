f3af8444b6dc9e55a2d83e9f8cd01ec3d85ae1b5
avanghelovici <artur.vanghelovici@endava.com>
2019-12-20 21:43:35 +0200

    Very Last COMMIT

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/testResources/features"},
         glue = {"stepDefinitions"},
-        tags = {"@Artur"},
+        tags = {"@Artur, @Mihail, @Anatol"},
         plugin = {"pretty", "html:target/cucumber-reports/"})
 public class Runner {
 
