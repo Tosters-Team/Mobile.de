bbc82b7886589f971bc35fd2eae508ec468f7ae0
anRailean <railean.anatolie99@gmail.com>
2020-05-26 22:09:10 +0300

    Added the new chromedriver.exe

--- src/test/java/Runner.java
+++ src/test/java/Runner.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/testResources/features"},
         glue = {"stepDefinitions"},
-        tags = {"@Artur, @Mihail, @Anatol"},
+        tags = {"@Anatol"},
         plugin = {"pretty", "html:target/cucumber-reports/"})
 public class Runner {
 
